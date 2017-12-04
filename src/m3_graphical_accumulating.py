946324bdb5b915ee887a07612129b9a49981fd2d
szalayvm <szalayvm>
2017-12-04 10:40:29 -0500

    I have started the first TODO.

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Victoria Szalay.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -94,6 +94,7 @@ def draw_parallel_lines(n, point, length, window):
       :type point: rg.Point
       :type length: int
       :type window: rg.RoseWindow
+
     """
     # ------------------------------------------------------------------
     # TODO: 2. Implement and test this function.
@@ -109,8 +110,13 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ####################################################################
     # ------------------------------------------------------------------
+    window1 = rg.RoseWindow(600,350)
+    window2 = rg.RoseWindow(500,400)
 
-
+    endpoint = rg.Point(point.x,length)
+    for k in range(n):
+        rg.Line(point, endpoint)
+        
 def run_test_draw_lines():
     """ Tests the   draw_lines  function. """
     print()
